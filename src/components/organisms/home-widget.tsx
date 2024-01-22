88c81dae7e8d2c3601565eac79d28337236c4c63
Yusril Izza <yusril@izza.dev>
2024-01-22 07:11:34 +0700

    feat: add responsive on home screen

--- src/components/organisms/home-widget.tsx
+++ src/components/organisms/home-widget.tsx
@@ -12,43 +12,55 @@ export function HomeWidget() {
 
   return (
     <div className="fixed left-0 top-0 z-20 w-full">
-      <div className="bg-primary-gradient h-[180px] w-full px-6 py-8">
-        <img
-          src="/assets/vector/line.svg"
-          alt="line-vector"
-          className="absolute left-0 top-0 w-full"
+      <div className="bg-primary-gradient flex h-[180px] w-full justify-center px-6 py-8">
+        <div
+          style={{
+            backgroundImage: `url('/assets/vector/line.svg')`,
+            backgroundRepeat: 'no-repeat',
+            backgroundSize: 'cover',
+            position: 'absolute',
+            left: 0,
+            top: 0,
+            zIndex: 0,
+            width: '100%',
+            height: '100%',
+          }}
         />
-        <div className="flex items-center justify-between">
-          <div className="flex">
-            <img alt="agpaii" className="-mt-7" src="/assets/logo/agpaii-logo-only.svg" />
-            <div className="ml-4 flex flex-col gap-1">
-              <h1 className="font-semibold text-white">Rumah AGPAII</h1>
-              <p className="text-xs font-medium text-white">Assalamualaikum {user?.name}</p>
-              <p className="text-xs font-medium text-white">
-                {dayjs().format('dddd, DD MMMM YYYY')}
-              </p>
+        <div className="w-full max-w-screen-md">
+          <div className="flex items-center justify-between">
+            <div className="flex">
+              <img alt="agpaii" className="-mt-7" src="/assets/logo/agpaii-logo-only.svg" />
+              <div className="ml-4 flex flex-col gap-1">
+                <h1 className="font-semibold text-white">Rumah AGPAII</h1>
+                <p className="text-xs font-medium text-white">Assalamualaikum {user?.name}</p>
+                <p className="text-xs font-medium text-white">
+                  {dayjs().format('dddd, DD MMMM YYYY')}
+                </p>
+              </div>
             </div>
+            <Link className="relative z-30" to="/profile">
+              <img
+                className="h-12 w-12 rounded-full"
+                src={`https://i.pravatar.cc/150?img=${2}`}
+                alt="avatar"
+              />
+            </Link>
           </div>
-          <Link className="relative z-30" to="/profile">
-            <img
-              className="h-12 w-12 rounded-full"
-              src={`https://i.pravatar.cc/150?img=${2}`}
-              alt="avatar"
-            />
-          </Link>
         </div>
       </div>
-      <div className="px-6">
-        <div
-          className="flex w-full items-center rounded-xl bg-white px-4 py-5 shadow-lg"
-          style={{ marginTop: -60 }}
-        >
-          <IonIcon className="size-5 text-primary" icon={location} />
-          <div className="ml-3 flex flex-col">
-            <p className="text-xs font-semibold">
-              {city}, {state}
-            </p>
-            <p className="mt-1 text-xs">{nextPrayerTime}</p>
+      <div className="flex w-full justify-center px-6">
+        <div className="w-full max-w-screen-md">
+          <div
+            className="flex w-full items-center rounded-xl bg-white px-4 py-5 shadow-lg"
+            style={{ marginTop: -60 }}
+          >
+            <IonIcon className="size-5 text-primary" icon={location} />
+            <div className="ml-3 flex flex-col">
+              <p className="text-xs font-semibold">
+                {city}, {state}
+              </p>
+              <p className="mt-1 text-xs">{nextPrayerTime}</p>
+            </div>
           </div>
         </div>
       </div>
--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -21,108 +21,110 @@ export default function Home() {
         <HomeWidget />
       </IonHeader>
       <IonContent>
-        <div className="mt-[210px]">
-          <IonGrid className="mt-4 w-full pb-4 shadow-md">
-            <IonRow>
-              <IonCol size="3">
-                <HomeMenu icon="rumah-agpaii" to="/agpaii-home" title="Rumah AGPAII" />
-              </IonCol>
-              <IonCol size="3">
-                <HomeMenu icon="kta-digital" to="/kta-digital" title="KTA Digital" />
-              </IonCol>
-              <IonCol size="3">
-                <HomeMenu icon="info-anggota" to="/member-information" title="Info Anggota" />
-              </IonCol>
-              <IonCol size="3">
-                <HomeMenu
-                  icon="struktur-organisasi"
-                  to="/organization-structure"
-                  title="Struktur Organisasi"
-                />
-              </IonCol>
-            </IonRow>
-            <IonRow className="mt-4">
-              <IonCol size="3">
-                <HomeMenu icon="alquran" to="/alquran" title="Al-Qur'an" />
-              </IonCol>
-              <IonCol size="3">
-                <HomeMenu icon="event" to="/events" title="Event / Acara" />
-              </IonCol>
-              <IonCol size="3">
-                <HomeMenu icon="digital-module" to="/agpaii-module" title="Modul Digital" />
-              </IonCol>
-            </IonRow>
-          </IonGrid>
-
-          <div className="px-6 py-6 shadow-md">
-            <Swiper pagination spaceBetween={50} slidesPerView={1} modules={[Pagination]}>
-              <SwiperSlide>
-                <div className="w-full shadow-sm">
-                  <img
-                    className="h-[156px] w-full rounded-xl object-cover"
-                    height={156}
-                    src={`https://source.unsplash.com/random/1200x156?sig=${dayjs().unix()}`}
-                    alt="slide"
+        <div className="mt-[210px] flex w-full justify-center">
+          <div className="w-full max-w-screen-md">
+            <IonGrid className="relative z-20 mt-4 w-full pb-4 shadow-md">
+              <IonRow>
+                <IonCol size="3">
+                  <HomeMenu icon="rumah-agpaii" to="/agpaii-home" title="Rumah AGPAII" />
+                </IonCol>
+                <IonCol size="3">
+                  <HomeMenu icon="kta-digital" to="/kta-digital" title="KTA Digital" />
+                </IonCol>
+                <IonCol size="3">
+                  <HomeMenu icon="info-anggota" to="/member-information" title="Info Anggota" />
+                </IonCol>
+                <IonCol size="3">
+                  <HomeMenu
+                    icon="struktur-organisasi"
+                    to="/organization-structure"
+                    title="Struktur Organisasi"
                   />
-                </div>
-              </SwiperSlide>
-              <SwiperSlide>
-                <div className="w-full shadow-sm">
-                  <img
-                    className="h-[156px] w-full rounded-xl object-cover"
-                    height={156}
-                    src={`https://source.unsplash.com/random/1200x156?sig=${Math.random().toFixed(
-                      2,
-                    )}`}
-                    alt="slide"
-                  />
-                </div>
-              </SwiperSlide>
-            </Swiper>
-          </div>
+                </IonCol>
+              </IonRow>
+              <IonRow className="mt-4">
+                <IonCol size="3">
+                  <HomeMenu icon="alquran" to="/alquran" title="Al-Qur'an" />
+                </IonCol>
+                <IonCol size="3">
+                  <HomeMenu icon="event" to="/events" title="Event / Acara" />
+                </IonCol>
+                <IonCol size="3">
+                  <HomeMenu icon="digital-module" to="/agpaii-module" title="Modul Digital" />
+                </IonCol>
+              </IonRow>
+            </IonGrid>
 
-          <div className="px-6 py-6 shadow-md">
-            <h5 className="font-medium">Artikel Terbaru</h5>
-
-            <div className="mt-4 pb-4">
-              {article?.data?.[0] && (
-                <ArticleCard
-                  title={article?.data?.[0]?.title}
-                  to={`/article/${encodeURIComponent(article?.data?.[0].slug || '')}`}
-                />
-              )}
-
-              <div className="mt-4">
-                <IonGrid>
-                  <IonRow>
-                    {article?.data?.slice(1, 3)?.map((article, i) => (
-                      <IonCol size="6" key={i}>
-                        <ArticleCard
-                          fullWidth
-                          variant="vertical"
-                          title={article.title}
-                          to={`/article/${encodeURIComponent(article.slug || '')}`}
-                        />
-                      </IonCol>
-                    ))}
-                  </IonRow>
-                </IonGrid>
-              </div>
+            <div className="px-6 py-6 shadow-md">
+              <Swiper pagination spaceBetween={50} slidesPerView={1} modules={[Pagination]}>
+                <SwiperSlide>
+                  <div className="w-full shadow-sm">
+                    <img
+                      className="h-[156px] w-full rounded-xl object-cover"
+                      height={156}
+                      src={`https://source.unsplash.com/random/1200x156?sig=${dayjs().unix()}`}
+                      alt="slide"
+                    />
+                  </div>
+                </SwiperSlide>
+                <SwiperSlide>
+                  <div className="w-full shadow-sm">
+                    <img
+                      className="h-[156px] w-full rounded-xl object-cover"
+                      height={156}
+                      src={`https://source.unsplash.com/random/1200x156?sig=${Math.random().toFixed(
+                        2,
+                      )}`}
+                      alt="slide"
+                    />
+                  </div>
+                </SwiperSlide>
+              </Swiper>
             </div>
 
-            <div className="mt-4">
-              <Swiper spaceBetween={50} slidesPerView={1} modules={[Pagination]}>
-                {article?.data?.slice(3, 11)?.map((article, i) => (
-                  <SwiperSlide key={i}>
-                    <ArticleCard
-                      fullWidth
-                      variant="vertical"
-                      title={article.title}
-                      to={`/article/${encodeURIComponent(article.slug || '')}`}
-                    />
-                  </SwiperSlide>
-                ))}
-              </Swiper>
+            <div className="px-6 py-6 shadow-md">
+              <h5 className="font-medium">Artikel Terbaru</h5>
+
+              <div className="mt-4 pb-4">
+                {article?.data?.[0] && (
+                  <ArticleCard
+                    title={article?.data?.[0]?.title}
+                    to={`/article/${encodeURIComponent(article?.data?.[0].slug || '')}`}
+                  />
+                )}
+
+                <div className="mt-4">
+                  <IonGrid>
+                    <IonRow>
+                      {article?.data?.slice(1, 3)?.map((article, i) => (
+                        <IonCol size="6" key={i}>
+                          <ArticleCard
+                            fullWidth
+                            variant="vertical"
+                            title={article.title}
+                            to={`/article/${encodeURIComponent(article.slug || '')}`}
+                          />
+                        </IonCol>
+                      ))}
+                    </IonRow>
+                  </IonGrid>
+                </div>
+              </div>
+
+              <div className="mt-4">
+                <Swiper spaceBetween={50} slidesPerView={1} modules={[Pagination]}>
+                  {article?.data?.slice(3, 11)?.map((article, i) => (
+                    <SwiperSlide key={i}>
+                      <ArticleCard
+                        fullWidth
+                        variant="vertical"
+                        title={article.title}
+                        to={`/article/${encodeURIComponent(article.slug || '')}`}
+                      />
+                    </SwiperSlide>
+                  ))}
+                </Swiper>
+              </div>
             </div>
           </div>
         </div>
